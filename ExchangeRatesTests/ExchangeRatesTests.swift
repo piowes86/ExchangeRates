dda249daa148c8fe5eff4350431ebe8c5b99d934
Piotr Wesolowski <pwesolowski@printoscope.pl>
2020-04-09 12:10:24 +0200

    Initial Commit

---
+++ ExchangeRatesTests/ExchangeRatesTests.swift
@@ -0,0 +1,34 @@
+//
+//  ExchangeRatesTests.swift
+//  ExchangeRatesTests
+//
+//  Created by Piotr Wesolowski on 09/04/2020.
+//  Copyright © 2020 Piotr Wesolowski. All rights reserved.
+//
+
+import XCTest
+@testable import ExchangeRates
+
+class ExchangeRatesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
